feb1aca4a9d82bb90e64f5eb10876e6baf5ebb4d
wosasie <i44aul@gmail.com>
2025-03-10 23:50:06 +0800

    pertemuan

---
+++ kelas/pertemuan-1/pertemuan-1.cpp
@@ -0,0 +1,50 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main(){
+    // cout << "helo saciw!" << endl;
+    // return 0
+
+    // string nama;
+    // cout << "Masukan nama kamu: ";
+    // cin >> nama;
+    // cout << "halo, " << nama;
+    // return 0
+
+    // bool sudah_makan = true;
+    // string status = sudah_makan ? "sudah makan" : "belum makan";
+    // cout << status << endl;
+
+    // bool rehan = true;
+    // bool rajin = false;
+    // if (rehan){
+    //     cout << "nama rehan" << endl;
+    //     if (rajin){
+    //         cout << "rehan rajin" << endl;
+    //     } else {
+    //         cout << "bukan rehan"
+    //     }
+    // }
+
+    // for (int i = 20; i > 1 ; i-- ){
+    //     cout << i << endl;
+    // }
+
+    // int angka = 0;
+    // while (true){
+    //     cout << "perulangan-" << angka << endl;
+    //     angka++;
+    // }
+
+    // int nilai = 0;
+    // do {
+    //     cout << nilai;
+    //     nilai++;
+    // } while (nilai > 10);
+
+        // while (nilai > 90){
+        //     cout << "nilai";
+        //     nilai++;
+        // }
+// }
